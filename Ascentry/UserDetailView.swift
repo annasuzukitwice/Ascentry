523e3fc714c8e50973781b5efa64ee45b5b11c89
Anna Suzuki <ans.omlette1123@gmail.com>
2025-04-18 19:37:56 +0900

    user画面、userdetail画面の基礎

---
+++ Ascentry/UserDetailView.swift
@@ -0,0 +1,144 @@
+//
+//  UserDetailView.swift
+//  Ascentry
+//
+//  Created by 鈴木杏南 on 2025/02/21.
+//
+
+import SwiftUI
+
+
+struct UserDetailView: View {
+    @State private var userInput: String = ""
+    @State private var userInput2: String = ""
+    @State private var selectedView: ViewType = .first
+    
+    var body: some View {
+        ZStack {
+            
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image("Usericon")
+                    .resizable()
+                    .frame(width: 230.0, height: 230)
+                    .scaledToFill()
+                
+                HStack {
+                    
+                    Image("username")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    
+                    TextField("Username", text: $userInput)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .padding(.top, 20)  // ユーザーアイコンと少し間隔をあける
+                        .foregroundColor(.black)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Image("Edit2")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    
+                }
+                
+                HStack {
+                    
+                    Image("userid")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    
+                    TextField("UserID", text: $userInput2)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .padding(.top, 20)  // ユーザーアイコンと少し間隔をあける
+                        .foregroundColor(.black)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Image("Edit2")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                }
+                
+                HStack {
+                    
+                    Image("Pin1")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    
+                    TextField("Location", text: $userInput)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .padding(.top, 20)  // ユーザーアイコンと少し間隔をあける
+                        .foregroundColor(.black)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Image("Edit2")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                }
+            }
+            .navigationTitle("User2")  // 次の画面のタイトル
+        }
+        
+        VStack {
+            // 横並びのボタン
+            HStack {
+                Button(action: {
+                    selectedView = .first
+                }) {
+                    Text("✔︎")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .cornerRadius(8)
+                }
+                
+                Button(action: {
+                    selectedView = .second
+                }) {
+                    Text("×")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+        }
+    }
+}
+
+// 表示する画面のタイプ
+enum ViewType {
+    case first
+    case second
+}
+
+// 切り替えるビュー
+struct FirstView: View {
+    var body: some View {
+        Text("これは画面1です")
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        Text("これは画面2です")
+            .font(.largeTitle)
+            .foregroundColor(.green)
+    }
+}
+
+    #Preview {
+        UserDetailView()
+    }
--- Ascentry/UserView.swift
+++ Ascentry/UserView.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct UserView: View {
     var body: some View {
-        ZStack{
-            Color.purple
-            Text("ユーザー画面")
+        NavigationStack{
+            VStack(spacing: 0){
+                ZStack(alignment: .topTrailing) {
+                    
+                    Image("user")
+                        .resizable()
+                        .frame(width: 393.0, height: .infinity)
+                        .scaledToFit()
+                    
+                    NavigationLink(destination: UserDetailView()) {
+                        Image("Edit")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 39, height: 39)
+                            .padding(EdgeInsets(
+                                top: 14,        // 上の余白
+                                leading: 300,    // 左の余白
+                                bottom: 0,     // 下の余白
+                                trailing: 18    // 右の余白
+                            ))
+                        
+                    }
+                }
+            }
+            
+            ZStack{
+                Color.black
+            }
         }
     }
 }
+
 
 #Preview {
     UserView()
