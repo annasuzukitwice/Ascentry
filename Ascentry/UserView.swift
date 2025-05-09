56366a92ce20f1afcf6ec5ef7d4f44f34db539f5
Anna Suzuki <ans.omlette1123@gmail.com>
2025-05-09 20:56:00 +0900

    userviewのボタン配置

--- Ascentry/UserView.swift
+++ Ascentry/UserView.swift
@@ -8,36 +8,186 @@
 import SwiftUI
 
 struct UserView: View {
+    @State private var message4: String = "username"
+    @State private var message5: String = "location"
+    @State private var message6: String = "Sound Title"
+    @State private var message7: String = "User Name"
+    @State private var message8: String = "location"
     var body: some View {
         NavigationStack{
-            VStack(spacing: 0){
-                ZStack(alignment: .topTrailing) {
-                    
+            VStack(spacing: 10){
+                ZStack {
                     Image("user")
                         .resizable()
-                        .frame(width: 393.0, height: .infinity)
                         .scaledToFit()
+                        .frame(width: 393, height: 393)
+
+                    VStack {
+                        HStack {
+                            Spacer()
+                            NavigationLink(destination: UserDetailView()) {
+                                Image("Edit")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 39, height: 39)
+                                    .padding(.top, 14)
+                                    .padding(.trailing, 18)
+                            }
+                        }
+                        Spacer()
+                    }
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Text(message4)
+                                .foregroundColor(.white)
+                                .padding(.trailing, 120)
+                                .padding(.leading, 10)
+                                .padding(.bottom, 10)
+                                .font(.system(size: 50))
+                                .bold()
+                                .italic()
+
+                            Spacer()
+                        }
+                    }
+                }
+                
+                HStack {
+                    Image("username")
+                        .padding(.leading, 30)
+                    Text(message4)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 120)
                     
-                    NavigationLink(destination: UserDetailView()) {
-                        Image("Edit")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 39, height: 39)
-                            .padding(EdgeInsets(
-                                top: 14,        // 上の余白
-                                leading: 300,    // 左の余白
-                                bottom: 0,     // 下の余白
-                                trailing: 18    // 右の余白
-                            ))
+                    Spacer()
+                
+                }
+                
+                HStack {
+                    Image("Pin1")
+                        .padding(.leading, 30)
+                    Text(message5)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 120)
+                    Spacer()
+                
+                }
+                .padding(.bottom, 10)
+                HStack {
+                    Text("LIBRARY")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                        .padding(.leading)
+                        .bold()
+                    Spacer()
+                    
+                }
+                
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 350, height: 70)
                         
+                        HStack(spacing: 35) {
+                            Image("image")
+                                .resizable()
+                                .frame(width: 56, height: 56)
+                                .scaledToFit()
+                            VStack {
+                                Text(message6)
+                                    .font(.system(size: 20))
+                                HStack {
+                                    Image("usernameblack")
+                                        .frame(width: 10, height: 10)
+                                        .scaledToFit()
+                                    Text(message7)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Image("Pin2")
+                                    Text(message8)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                        }
+                    }
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 350, height: 70)
+                        
+                        HStack(spacing: 35) {
+                            Image("image")
+                                .resizable()
+                                .frame(width: 56, height: 56)
+                                .scaledToFit()
+                            VStack {
+                                Text(message6)
+                                    .font(.system(size: 20))
+                                HStack {
+                                    Image("usernameblack")
+                                        .frame(width: 10, height: 10)
+                                        .scaledToFit()
+                                    Text(message7)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Image("Pin2")
+                                    Text(message8)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                        }
+                    }
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 350, height: 70)
+                        
+                        HStack(spacing: 35) {
+                            Image("image")
+                                .resizable()
+                                .frame(width: 56, height: 56)
+                                .scaledToFit()
+                            VStack {
+                                Text(message6)
+                                    .font(.system(size: 20))
+                                HStack {
+                                    Image("usernameblack")
+                                        .frame(width: 10, height: 10)
+                                        .scaledToFit()
+                                    Text(message7)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Image("Pin2")
+                                    Text(message8)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                        }
                     }
                 }
             }
+            .background(Color.black)
             
             ZStack{
                 Color.black
             }
+            .background(Color.black)
         }
+        .background(Color.black)
     }
 }
 
